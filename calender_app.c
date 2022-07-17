1bfe1ed4faeb6927599a2583b547c6ffb9893a5e
tanverrana <tanversobur@gmail.com>
2022-07-17 20:42:51 +0600

    print month name

--- calender_app.c
+++ calender_app.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char *month[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int i;
+    for (i = 0; i < 12; i++)
+    {
+        printf("\n-----------------%s --------------------\n", months[i]);
+    }
     return 0;
 }
