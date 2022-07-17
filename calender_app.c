8461afbdbff03693eb546375364149629d6c0901
tanverrana <tanversobur@gmail.com>
2022-07-17 20:39:37 +0600

    start project

---
+++ calender_app.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    char *month[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    return 0;
+}
